63673ea800ac3692d81f67111003414f354d6672
Tiago <tjdarosa1@gmail.com>
2022-11-06 15:38:16 -0300

    ResignationView done

--- package/app/api/modules/resignation/dto/ResignationDto.py
+++ package/app/api/modules/resignation/dto/ResignationDto.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass, field
 from typing import Optional
-from datetime import date
 from package.app.api.model.Employee import Employee
 from package.app.api.model.ResignationType import ResignationType
 
@@ -9,7 +8,7 @@ class ResignationDto:
     id: Optional[int] = field(default=None)
     employee_id: Optional[int] = field(default=None)
     resignation_type_id: Optional[int] = field(default=None)
-    date: Optional[date] = field(default=None)
+    date: Optional[str] = field(default=None)
     memo: Optional[str] = field(default=None)
     employee: Optional[Employee] = field(default=None)
     resignation_type: Optional[ResignationType] = field(default=None)
--- package/app/client/modules/resignation/ResignationView.py
+++ package/app/client/modules/resignation/ResignationView.py
@@ -10,20 +10,25 @@ from package.app.client.gui.imports import Gtk
 class ResignationView(metaclass=Singleton):
     def __init__(self):
         self.__component = ResignationComponent()
-        self.__EmployeeCombo = Gtk.Widget
-        self.__ResignationTypeCombo = Gtk.Widget
+        self.__employeeCombo = Gtk.Widget
+        self.__resignationTypeCombo = Gtk.Widget
+        self.__memoInput = Gtk.Widget
 
     def get(self) -> Gtk.Box:
 
         mainBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 
+
+        # MAIN LABEL  
         label = Gtk.Label()
         label.set_markup(toBig("Demitir Funcionário"))
         label.set_margin_bottom(30)
 
 
-
+        # EMPLOYEE BOX
         employeeBox = Box(Gtk.Orientation.HORIZONTAL)
+        employeeBox.set_margin_bottom(5)
+        employeeBox.set_margin_top(5)
         
         employeeLabel = Gtk.Label()
         employeeLabel.set_markup("Funcionário*:")
@@ -32,47 +37,76 @@ class ResignationView(metaclass=Singleton):
         employeeCombo = Gtk.ComboBoxText()
         employeeCombo.set_entry_text_column(0)
         employeeCombo.append_text("Selecione um funcionário")
-        employeeCombo.append_text("Selecione um funcionário2")
+        employeeCombo.append_text("Selecione outro funcionário")
         employeeCombo.set_active(0)
-        employeeCombo.new()
+        employeeCombo.set_size_request(275,30)
         employeeBox.pack_default(employeeCombo)
         
 
+        # RESIGNATION TYPE BOX
+        resignationTypeBox = Box(Gtk.Orientation.HORIZONTAL)
+        resignationTypeBox.set_margin_bottom(5)
+        resignationTypeBox.set_margin_top(5)
 
-        jobValueBox = Box(Gtk.Orientation.HORIZONTAL)
-       
-        jobValueLabel = Gtk.Label()
-        jobValueLabel.set_text("Preço do serviço*")
-        jobValueLabel.set_margin_top(5)
-        jobValueLabel.set_margin_left(5)
-        jobValueLabel.set_margin_bottom(5)
-        jobValueLabel.set_margin_right(5)
+        resignationTypeLabel = Gtk.Label()
+        resignationTypeLabel.set_markup("Tipo de causa*:")
+        resignationTypeBox.pack_default(resignationTypeLabel)
         
-        jobValueBox.pack_default(jobValueLabel)
-        jobValueInput = Gtk.Entry()
-        jobValueInput.set_margin_top(5)
-        jobValueInput.set_margin_left(5)
-        jobValueInput.set_margin_bottom(5)
-        jobValueInput.set_margin_right(5)
-        jobValueBox.pack_default(jobValueInput)
+        resignationTypeCombo = Gtk.ComboBoxText()
+        resignationTypeCombo.set_entry_text_column(0)
+        resignationTypeCombo.append_text("Selecione uma causa")
+        resignationTypeCombo.append_text("Selecione outra causa")
+        resignationTypeCombo.set_active(0)
+        resignationTypeCombo.set_size_request(285,30)
+        resignationTypeBox.pack_default(resignationTypeCombo)
+
+
+        # MEMO BOX
+        memoBox = Box(Gtk.Orientation.HORIZONTAL)
+        memoBox.set_margin_top(5)
+        memoBox.set_margin_bottom(5)
+       
+        memoLabel = Gtk.Label()
+        memoLabel.set_text("Observação:")
+        
+        memoBox.pack_default(memoLabel)
+        memoInput = Gtk.Entry()
+        memoInput.set_size_request(268,90)
+        memoBox.pack_default(memoInput)
+
+
+        # BUTTONS BOX
+        ButtonsBox = Box(Gtk.Orientation.HORIZONTAL)
+        ButtonsBox.set_margin_top(30)
+
+        goBackButton = Gtk.Button(label="Voltar")
+        goBackButton.set_margin_right(15)
+        goBackButton.set_size_request(100,30)
+        ButtonsBox.pack_default(goBackButton)
 
         confirmButton = Gtk.Button(label="Cadastrar")
-        confirmButton.set_margin_top(30)
+        confirmButton.set_margin_left(15)
+        confirmButton.set_size_request(100,30)
         confirmButton.connect("clicked", self.__onConfirm)
+        ButtonsBox.pack_default(confirmButton)
 
-        self.__EmployeeCombo = employeeCombo
-        self.__jobValueInput = jobValueInput
+
+        self.__employeeCombo = employeeCombo
+        self.__resignationTypeCombo = resignationTypeCombo
+        self.__memoInput = memoInput
 
         mainBox.pack_start(label, False, False, 0)
         mainBox.pack_start(employeeBox, False, False, 0)
-        mainBox.pack_start(jobValueBox, False, False, 0)
-        mainBox.pack_start(confirmButton, False, False, 0)
+        mainBox.pack_start(resignationTypeBox, False, False, 0)
+        mainBox.pack_start(memoBox, False, False, 0)
+        mainBox.pack_start(ButtonsBox, False, False, 0)
 
         return mainBox
 
     def __onConfirm(self, _: Gtk.Widget):
-        self.__component.getState().addReference("jobName", self.__EmployeeCombo)
-        self.__component.getState().addReference("jobValue", self.__ResignationTypeCombo)
+        self.__component.getState().addReference("employee", self.__employeeCombo)
+        self.__component.getState().addReference("resignationType", self.__resignationTypeCombo)
+        self.__component.getState().addReference("memo", self.__memoInput)
 
         self.__component.requestRegistration()
 
